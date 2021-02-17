2d7cd29d0e2e4534cf341c5bdfb853061db8bba5
TheusN <matheusnattan8@gmail.com>
2021-02-17 08:23:32 -0300

    Projeto sistema APK

---
+++ app/src/main/java/com/example/vendedor/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.vendedor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity<shouldOver> extends AppCompatActivity {
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView mWebView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://vendedor.sementesjosias.com/");
+        mWebView.setWebViewClient(new WebViewClient());
+
+    }
+}
